5de3d81a1a17d199f39aa2974a943cceb546c24e
Vladislav Matveev <leucist@gmail.com>
2025-08-03 19:18:39 +0300

    Add getter to Entity's id field

--- Application/Entities/Entity.cs
+++ Application/Entities/Entity.cs
@@ -2,6 +2,9 @@ namespace Application.Entities
 {
     public readonly struct Entity(int id)
     {
+        // Entity may be deprecated to typedef of int if not expanded further
         private readonly int _id = id;
+
+        public int Id => _id;
     }
 }
