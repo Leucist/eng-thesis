1083341407d6d9668578cf0278a229026e3d2add
Vladislav Matveev <leucist@gmail.com>
2025-08-03 14:11:23 +0300

    Basic game classes initialized

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Application 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
