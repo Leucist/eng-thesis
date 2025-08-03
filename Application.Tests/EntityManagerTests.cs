60ae8326a5e6b362327b64152388c629bddcc712
Vladislav Matveev <leucist@gmail.com>
2025-08-03 13:50:41 +0300

    Add basic tests of Entity-Component interactions

--- Application.Tests/EntityManagerTests.cs
+++ Application.Tests/EntityManagerTests.cs
@@ -1,43 +1,47 @@
 using Application.Entities;
+using Application.Components;
 
 namespace Application.Tests;
 
 public class EntityManagerTests
 {
+    private readonly EntityManager _entityManager = EntityManager.Instance;
+    private readonly Entity _entity = EntityManager.Instance.CreateEntity();
+
     [Fact]
     public void CreateEntity_ShouldReturnUniqueIds()
     {
-        var entityManager = EntityManager.Instance;
+        var entity2 = _entityManager.CreateEntity();
         
-        var entity1 = entityManager.CreateEntity();
-        var entity2 = entityManager.CreateEntity();
-        
-        Assert.NotEqual(entity1, entity2);
+        Assert.NotEqual(_entity, entity2);
     }
 
     [Fact]
     public void CreateEntity_EntitiesShouldBeContained()
     {
-        var entityManager = EntityManager.Instance;
+        var entity2 = _entityManager.CreateEntity();
         
-        var entity1 = entityManager.CreateEntity();
-        var entity2 = entityManager.CreateEntity();
-        
-        Assert.True(entityManager.EntityExists(entity1));
-        Assert.True(entityManager.EntityExists(entity2));
+        Assert.True(_entityManager.EntityExists(_entity));
+        Assert.True(_entityManager.EntityExists(entity2));
     }
 
-    // [Fact]
-    // public void AddComponent_ShouldStoreAndRetrieveComponent()
-    // {
-    //     var entityManager = new EntityManager();
-    //     var entity = entityManager.CreateEntity();
-    //     var position = new PositionComponent(10f, 20f);
+    [Fact]
+    public void AddComponent_ShouldStoreAndRetrieveComponent()
+    {
+        var transform = new TransformComponent(10f, 20f, 30f, 40f);
         
-    //     entityManager.AddComponent(entity, position);
-    //     var retrieved = entityManager.GetComponent<PositionComponent>(entity);
+        _entityManager.AddComponent(_entity, transform);
+        var retrieved = _entityManager.GetComponent<TransformComponent>(_entity);
         
-    //     Assert.Equal(position, retrieved);
-    //     Assert.True(entityManager.HasComponent<PositionComponent>(entity));
-    // }
+        Assert.Equal(transform, retrieved);
+        Assert.True(_entityManager.HasComponent<TransformComponent>(_entity));
+    }
+
+    [Fact]
+    public void RemoveComponent_ShouldRemoveComponent()
+    {        
+        _entityManager.RemoveComponent<TransformComponent>(_entity);
+        
+        Assert.False(_entityManager.HasComponent<TransformComponent>(_entity));
+    }
 }
--- Application/Entities/EntityManager.cs
+++ Application/Entities/EntityManager.cs
@@ -1,3 +1,5 @@
+using Application.Components;
+
 namespace Application.Entities
 {
     public class EntityManager
@@ -6,7 +8,7 @@ namespace Application.Entities
         private static readonly object _lock = new();   // Lock object for thread-safe singleton instantiation
 
         private int _lastID;
-        private List<Entity> _entities;
+        private readonly Dictionary<Entity, List<IComponent>> _entities;
 
         private EntityManager()
         {
@@ -33,13 +35,39 @@ namespace Application.Entities
         public Entity CreateEntity()
         {
             var entity = new Entity(_lastID++);
-            _entities.Add(entity);
+            _entities.Add(entity, []);
             return entity;
         }
 
         public bool EntityExists(Entity entity)
         {
-            return _entities.Contains(entity);
+            return _entities.ContainsKey(entity);
+        }
+
+        public void AddComponent(Entity entity, IComponent component) {
+            if (!EntityExists(entity)) throw new Exception("Entity does not exist.");
+            _entities[entity].Add(component);
+        }
+
+        public void RemoveComponent<T>(Entity entity) where T : IComponent {
+            if (!EntityExists(entity)) throw new Exception("Entity does not exist.");
+
+            var componentToRemove = _entities[entity].FirstOrDefault(c => c is T);
+    
+            if (componentToRemove != null) {
+                _entities[entity].Remove(componentToRemove);
+            }
+        }
+
+        public bool HasComponent<T>(Entity entity) {
+            if (!EntityExists(entity)) throw new Exception("Entity does not exist.");
+            return _entities[entity].Any(c => c is T);
+        }
+
+        public T GetComponent<T>(Entity entity) {
+            if (!EntityExists(entity)) throw new Exception("Entity does not exist.");
+            var component = _entities[entity].FirstOrDefault(c => c is T) ?? throw new Exception($"Component of type {typeof(T)} does not exist for the entity.");
+            return (T)component;
         }
     }
 }
