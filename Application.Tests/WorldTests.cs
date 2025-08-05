8c16e69277354268a56d6aaeaa8bf15d46ea2909
Vladislav Matveev <leucist@gmail.com>
2025-08-05 18:32:14 +0300

    Add initial world tests

--- Application.Tests/WorldTests.cs
+++ Application.Tests/WorldTests.cs
@@ -1,14 +1,25 @@
+using Application.Entities;
+
 namespace Application.Tests
 {
     public class WorldTests
     {
         private WorldFactory _factory = new();
 
-        // [Fact]
-        // public void WorldHasPlayer() {
-        //     var world = _factory.InitialWorld;
+        [Fact]
+        public void InitialWorld_is_MainMenuWorld() {
+            var initWorld = _factory.InitialWorld;
+            var mmWorld = _factory.GetMainMenuWorld();
 
-        //     Assert.True(world.Player is not null);
-        // }
+            Assert.Equal(mmWorld, initWorld);
+        }
+
+        [Fact]
+        public void MainMenuWorld_HasCustomPlayer() {
+            var world = _factory.GetMainMenuWorld();
+            var player = EntityManager.Instance.GetMainMenuPlayer();
+
+            Assert.Equal(world.Player, player);
+        }
     }
 }
