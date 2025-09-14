6b56b70c6a8c34e3966314889775b87f769b8e44
Vladislav Matveev <leucist@gmail.com>
2025-09-14 12:44:18 +0300

    Prepared tests for the MovementSystem

--- Application.Tests/MovementSystemTests.cs
+++ Application.Tests/MovementSystemTests.cs
@@ -1,46 +1,46 @@
-// using Application.Systems;
-// using Application.Components;
-// using Application.Entities;
-// using Application.AppMath;
+using Application.Systems;
+using Application.Components;
+using Application.Entities;
+using Application.AppMath;
 
-// namespace Application.Tests
-// {
-//     public class MovementSystemTests
-//     {
-//         [Fact]
-//         public void EntityShouldBeAbleToMove() {
-//             EntityManager entityManager = EntityManager.Instance;
-//             Entity entity = entityManager.CreateEntity();
-//             MovementSystem system = new();
-//             int x = 10, y = 20;
-//             TransformComponent  transformComponent  = new(x, y, 10, 20);
-//             PhysicsComponent    physicsComponent    = new(10, 120);
-//             entityManager.AddComponent(entity, transformComponent);
-//             entityManager.AddComponent(entity, physicsComponent);
+namespace Application.Tests
+{
+    public class MovementSystemTests
+    {
+        [Fact]
+        public void EntityShouldBeAbleToMove() {
+            EntityManager entityManager = EntityManager.Instance;
+            Entity entity = entityManager.CreateEntity();
+            MovementSystem system = new();
+            float x = 10, y = 20;
+            TransformComponent  transformComponent  = new(x, y, 10, 20);
+            PhysicsComponent    physicsComponent    = new(10, 120);
+            entityManager.AddComponent(entity, transformComponent);
+            entityManager.AddComponent(entity, physicsComponent);
             
-//             physicsComponent.AddAppliedForce(new (10, 0));
-//             system.Update((transformComponent, physicsComponent));
+            physicsComponent.AddAppliedForce(new (10, 0));
+            system.Update();
 
-//             Assert.Equal(y, transformComponent.Y);  // Y stayed the same
-//             Assert.True(x < transformComponent.X);  // X increased as the entity moved to the right
-//         }
+            Assert.Equal(y, transformComponent.Y);  // Y stayed the same
+            Assert.True(x < transformComponent.X);  // X increased as the entity moved to the right
+        }
 
-//         [Fact]
-//         public void EntityShouldBeAbleToJump() {
-//             EntityManager entityManager = EntityManager.Instance;
-//             Entity entity = entityManager.CreateEntity();
-//             MovementSystem system = new();
-//             int x = 10, y = 20;
-//             TransformComponent  transformComponent  = new(x, y, 10, 20);
-//             PhysicsComponent    physicsComponent    = new(10, 120);
-//             entityManager.AddComponent(entity, transformComponent);
-//             entityManager.AddComponent(entity, physicsComponent);
+        [Fact]
+        public void EntityShouldBeAbleToJump() {
+            EntityManager entityManager = EntityManager.Instance;
+            Entity entity = entityManager.CreateEntity();
+            MovementSystem system = new();
+            float x = 10, y = 20;
+            TransformComponent  transformComponent  = new(x, y, 10, 20);
+            PhysicsComponent    physicsComponent    = new(10, 120);
+            entityManager.AddComponent(entity, transformComponent);
+            entityManager.AddComponent(entity, physicsComponent);
             
-//             physicsComponent.AddAppliedForce(new (10, 90.ToRadians()));
-//             system.Update((transformComponent, physicsComponent));
+            physicsComponent.AddAppliedForce(new (10, 90.ToRadians()));
+            system.Update();
 
-//             Assert.Equal(x, transformComponent.X);  // X stayed the same
-//             Assert.True(y > transformComponent.Y);  // Y decreased as the entity moved up
-//         }
-//     }
-// }
+            Assert.Equal(x, transformComponent.X);  // X stayed the same
+            Assert.True(y > transformComponent.Y);  // Y decreased as the entity moved up
+        }
+    }
+}
