807d48dc9374a492bc056b5238a46d52d51dcbe8
Vladislav Matveev <leucist@gmail.com>
2025-09-19 16:20:15 +0300

    Imported the PathFinder implementation from the previous project.
    Commented out the unused methods for now.

--- Application/PathFinder.cs
+++ Application/PathFinder.cs
@@ -1,7 +1,56 @@
 namespace Application
 {
-    public class PathFinder
+    public static class Pathfinder
     {
-        
+        public static string FindSolutionDirectory()
+        {
+            // Current directory of the executing process
+            string startDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var directory = new DirectoryInfo(startDirectory);
+
+            // Go up until we find solution file (.sln)
+            while (directory != null && !File.Exists(Path.Combine(directory.FullName, "Application.sln")))
+            {
+                directory = directory.Parent;
+            }
+
+            // Return path if it's found
+            if (directory != null)
+            {
+                return directory.FullName;
+            }
+
+            // Otherwise exception is thrown
+            throw new DirectoryNotFoundException("Solution root was not found.");
+        }
+
+        public static string GetGraphicsTexturesFolder() {
+            return Path.Combine(FindSolutionDirectory(), "Application", "Textures");
+        }
+        // public static string GetGraphicsFontsFolder() {
+        //     return Path.Combine(FindSolutionDirectory(), "Application", "fonts/Cinzel");
+        // }
+
+        // public static string GetIconPath() {
+        //     return Path.Combine(FindSolutionDirectory(), "Application", "images", "icon.png");
+        // }
+
+
+        // public static string GetCutsceneFolderPath(CutsceneType cutscene) {
+        //     string cutscenesFolder = "Cutscenes";
+        //     string cutsceneFolderName = "Cutscene_" + cutscene.ToString();
+        //     return Path.Combine(cutscenesFolder, cutsceneFolderName);
+        // }
+
+        // public static string GetFrameFilePath(int frameNumber, CutsceneType cutscene) {
+        //     string cutsceneFolderPath = GetCutsceneFolderPath(cutscene);
+        //     string frameFileName = frameNumber.ToString();
+        //     string frameFilePath = Path.Combine(cutsceneFolderPath, frameFileName);
+        //     return frameFilePath;
+        // }
+
+        // public static string GetAudioFolder() {
+        //     return Path.Combine(FindSolutionDirectory(), "Application", "Audio");
+        // }
     }
 }
