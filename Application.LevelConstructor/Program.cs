8c63bd35df1b21c08e59ddb84452559fbd9ae40a
Vladislav Matveev <leucist@gmail.com>
2025-09-22 21:30:59 +0300

    Added base for the LevelConstructor entry point

--- Application.LevelConstructor/Program.cs
+++ Application.LevelConstructor/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Application;
+
+namespace Application.LevelConstructor {
+    public static class Program {
+        public static void Main(string[] args) {
+            
+        }
+    }
+}
