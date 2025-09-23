60ed3d79f88a0f1c4b5d9b49d8991f2caa16d15e
Vladislav Matveev <leucist@gmail.com>
2025-09-23 10:21:52 +0300

    Basic window init

--- Application.LevelConstructor/Program.cs
+++ Application.LevelConstructor/Program.cs
@@ -1,9 +1,17 @@
 ﻿using Application;
+using SFML.Graphics;
+using SFML.Window;
 
 namespace Application.LevelConstructor {
     public static class Program {
+        private const string TITLE = "Level Constructor";
+
         public static void Main(string[] args) {
-            
+            RenderWindow window = new(new VideoMode(1920, 1080), TITLE, Styles.Fullscreen);
+
+            while (window.IsOpen) {
+                // do smth (or not)
+            }
         }
     }
 }
