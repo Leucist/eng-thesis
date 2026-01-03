72488d3fe71529e2d078f6025be624de80445dde
Vladislav Matveev <leucist@gmail.com>
2026-01-03 17:12:58 +0300

    [Mid-Fix] Intermidiate commit among collision fixes

--- Application/Systems/CollisionSystem.cs
+++ Application/Systems/CollisionSystem.cs
@@ -17,7 +17,7 @@ namespace Application.Systems
     {
         // Buffer storage for one-frame collisions check - gathers all collidable entities for future check in PerformSystemAction()
         private Dictionary<Entity, FloatRect> _collidableRects = [];
-        private Dictionary<Entity, (FloatRect, TransformComponent, PhysicsComponent?/*, bool*/)> _movedEntities = [];
+        private Dictionary<Entity, (FloatRect, TransformComponent, PhysicsComponent?, bool)> _movedEntities = [];
         private readonly FloatRect ZERO_RECT = new (0, 0, 0, 0);
 
         private void GatherCollidableRects() {
@@ -37,7 +37,7 @@ namespace Application.Systems
                 TransformComponent tc = (TransformComponent) entity.Value.First(c => c.Type == ComponentType.Transform);
                 if (tc.HasMoved) {
                     PhysicsComponent? phc = (PhysicsComponent?) entity.Value.FirstOrDefault(c => c.Type == ComponentType.Physics);
-                    _movedEntities.Add(entity.Key, (entityBounds, tc, phc/*, false*/));
+                    _movedEntities.Add(entity.Key, (entityBounds, tc, phc, false));
                 }
             }
         }
@@ -52,14 +52,14 @@ namespace Application.Systems
             // Check the left border
             if (bounds.Left < 0) {
                 entity.SetX(0);
-                return true;
+                return false;
             }
             // Check the right border
             if (bounds.Left + bounds.Width > AppConstants.CANVAS_WIDTH) {
                 entity.SetX(AppConstants.CANVAS_WIDTH - bounds.Width);
-                return true;
+                return false;
             }
-            return false;
+            return true;
         }
 
         public override void Update()
@@ -74,53 +74,83 @@ namespace Application.Systems
         }
 
         protected override void PerformSystemAction(Dictionary<ComponentType, Component> entityComponents) {}
-        protected void PerformCustomSystemAction(KeyValuePair<Entity, (FloatRect, TransformComponent, PhysicsComponent?/*, bool*/)> entity) {
+        protected void PerformCustomSystemAction(KeyValuePair<Entity, (FloatRect, TransformComponent, PhysicsComponent?, bool)> entity) {
             FloatRect           entityRect          = entity.Value.Item1;
             TransformComponent  transformComponent  = entity.Value.Item2;
             PhysicsComponent?   physicsComponent    = entity.Value.Item3;
-            /*bool                hasCollidedOnBottom = entity.Value.Item4;*/
+            bool                hasCollidedOnBottom = entity.Value.Item4;
 
-            // - Iterate through borders
-            if (FitInScreenBounds(entityRect, transformComponent)) {
+            // - Iterate through borders // todo: maybe place this check in the end?
+            if (!FitInScreenBounds(entityRect, transformComponent)) {
                 // Stop the entity
                 physicsComponent?.Stop();
             }
 
+            // ! Debug log
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"\n- - - COLLISION CHECK ROUND - - -");
+            Console.ResetColor();
+
             // - Iterate through entities
             foreach (var collidable in _collidableRects) {
-                if (collidable.Key == entity.Key) continue; // to skip checking collision with itself
-                var intersection = CheckCollision(entityRect, collidable.Value);
-                // IF collision occured
-                if (intersection != ZERO_RECT) {
-                    // Find direction of the intersection
-                    float deltaX = intersection.Width < intersection.Height ? intersection.Left : 0; // ! added Height below
-                    float deltaY = intersection.Height < intersection.Width ? intersection.Top + intersection.Height : 0;
-                    // float deltaX = intersection.Left;
-                    // float deltaY = intersection.Top;
+                // Skip the check if collidable is this entity (collision with itself)
+                if (collidable.Key == entity.Key) continue;
 
-                    // Deduce from which side did collision occure
-                    if (deltaX != 0)
-                    {
-                        // Offset depending on the sign of deltaX
-                        var xOffset = deltaX > transformComponent.X ? -intersection.Width-1 : intersection.Width+1;
-                        // ! transformComponent.SetX(deltaX+xOffset);
-                        transformComponent.ChangePostition(xOffset, 0);
+                // Find intersection area
+                var intersection = CheckCollision(entityRect, collidable.Value);
+                
+                // * If collision occured
+                if (intersection != ZERO_RECT) {
+                    
+                    // ! Debug log
+                    Console.WriteLine($"\n- Collision occured with entity {entity.Key.Id} and {collidable.Key.Id} >");
+                    Console.WriteLine($"intersection.Width: {intersection.Width}\nintersection.Height: {intersection.Height}");
+                    Console.WriteLine($"intersection.Top: {intersection.Top}\nintersection.Left: {intersection.Left}");
+                    Console.WriteLine($"Before fix\t-\tX: {entity.Value.Item2.X}\tY: {entity.Value.Item2.Y}");
+
+                    // If X offset is greater than on Y axis - rise (or lower) the entity
+                    if (intersection.Width > intersection.Height) {
+                        var intersectionY = intersection.Top /* + intersection.Height // - as it's already bottom for some reason */;
+                        float yPos;
+                        if (intersectionY >= transformComponent.Y) {
+                            // if entity collided with smth on the bottom
+                            yPos = intersection.Top - intersection.Height;
+                            physicsComponent?.Ground();
+                            hasCollidedOnBottom = true;
+
+                            // ! Debug log
+                            Console.WriteLine($"(entity {entity.Key.Id} touched floor - no longer falling)");
+                        }
+                        else {
+                            var collidableYPos = collidable.Value.Top /* + collidable.Value.Height */;
+                            yPos = collidableYPos + transformComponent.Height;
+                        }
+                        transformComponent.SetY(yPos);
+                    }
+
+
+                    else {
+                        var xPos = intersection.Left > transformComponent.X ? collidable.Value.Left - transformComponent.Width
+                                                                            : collidable.Value.Left + collidable.Value.Width;
+                        transformComponent.SetX(xPos);
                         // Stop the entity
                         physicsComponent?.Stop();
                     }
-                    if (deltaY != 0)
-                    {
-                        // Offset depending on the sign of deltaY
-                        var yOffset = deltaY > transformComponent.Y ? -intersection.Height-1 : intersection.Height+1;
-                        // ! transformComponent.SetY(deltaY+yOffset);
-                        transformComponent.ChangePostition(0, yOffset);
-                        // Set entity as no longer falling
-                        physicsComponent?.Ground();
-                        // hasCollidedOnBottom = true;
-                    }
+
+                    // ! Debug log
+                    Console.WriteLine($"After fix\t-\tX: {entity.Value.Item2.X}\tY: {entity.Value.Item2.Y}");
                 }
             }
-            // if (!hasCollidedOnBottom && physicsComponent is not null) physicsComponent.IsFalling = true;
+
+            // ! Debug log
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"\n- - - END - - -");
+            Console.ResetColor();
+
+            // If the entity has never collided on the bottom it is falling
+            if (!hasCollidedOnBottom && physicsComponent is not null) {physicsComponent.IsFalling = true;
+                // ! Debug log
+                Console.WriteLine($"(entity {entity.Key.Id} is falling now)");}
         }
 
         public static bool AreColliding(FloatRect a, FloatRect b) {
